22aeb1091167fa038f73af146f6aae65839bf508
Ilya Makarov <mortimer08@gmail.com>
2024-04-14 21:14:26 +0500

    Data host

--- app/common/Data.java
+++ app/common/Data.java
@@ -3,7 +3,7 @@ package common;
 import play.Play;
 
 public class Data {
-    public static final String HOST = Play.configuration.getProperty("application.host", "anodes.ru");
+    public static final String HOST = Play.configuration.getProperty("application.host", "http://anodes.ru");
     public static final String MAILER_FROM_ADDRESS = Play.configuration.getProperty("mail.smtp.user", "mortimer08@gmail.com)");
     public static final String MAILER_FROM_PERSONAL = Play.configuration.getProperty("mail.smtp.personal","Anodes Admin");
     public static final String DAYS_FOR_ACTIVATION = "3";
