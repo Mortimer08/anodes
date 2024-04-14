1ee627132ca22040c4396ea8af8b70557fd822d4
Ilya Makarov <mortimer08@gmail.com>
2024-04-14 21:16:05 +0500

    password change

---
+++ app/controllers/auth/PasswordReset.java
@@ -0,0 +1,14 @@
+package controllers.auth;
+
+import play.mvc.Controller;
+
+
+public class PasswordReset extends Controller {
+    public static void passwordChange(){
+        render();
+    }
+    public static void _passwordChange(){
+        render();
+    }
+}
+
