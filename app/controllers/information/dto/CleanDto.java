072af99dd0b25cb90a94033c4a97b180f6838362
Ilya Makarov <mortimer08@gmail.com>
2024-12-26 22:04:30 +0500

    clean dto

---
+++ app/controllers/information/dto/CleanDto.java
@@ -0,0 +1,12 @@
+package controllers.information.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class CleanDto {
+
+    public Date date;
+    public List<Long> cell;
+    public List<Long> take;
+
+}
