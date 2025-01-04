2de02a8bdcf8088d975527cbb28ea0e910fb3dd3
Ilya Makarov <mortimer08@gmail.com>
2025-01-04 11:32:44 +0500

    first-last date - day of month

---
+++ app/controllers/util/DateUtils.java
@@ -0,0 +1,31 @@
+package controllers.util;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+
+import java.util.Date;
+
+public class DateUtils {
+
+    public static Date monthFirstDate(final Date date) {
+        return LocalDate.fromDateFields(date).dayOfMonth().withMinimumValue().toDate();
+    }
+
+    public static Date monthLastDate(final Date date) {
+        return LocalDate.fromDateFields(date).dayOfMonth().withMaximumValue().toDate();
+    }
+
+    public static String monthFirstDay(final Date date) {
+        return LocalDate.fromDateFields(date).dayOfMonth().withMinimumValue()
+                .toString(DateTimeFormat.forPattern("yyyy-MM-dd"));
+    }
+
+    public static String monthLastDay(final Date date) {
+        return LocalDate.fromDateFields(date).dayOfMonth().withMaximumValue()
+                .toString(DateTimeFormat.forPattern("yyyy-MM-dd"));
+    }
+
+    public static Date plusDays(Date date, int days) {
+        return LocalDate.fromDateFields(date).plusDays(days).toDate();
+    }
+}
