ff6695a7616c8ceb8e82c69538635a893c6edcb6
Ilya Makarov <mortimer08@gmail.com>
2025-01-04 11:34:38 +0500

    find by filter

--- app/models/information/Vacuuming.java
+++ app/models/information/Vacuuming.java
@@ -1,12 +1,16 @@
 package models.information;
 
 import common.model.information.Event;
+import models.Team;
 import models.ground.Cell;
+import play.db.jpa.JPA;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Tuple;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "vacuuming")
@@ -29,6 +33,21 @@ public class Vacuuming extends Event {
 
     public static Vacuuming findLast(final Cell cell) {
         return find(" select v from Vacuuming v where v.cell = ?1 order by happened desc", cell).first();
+    }
+
+    public static List<Tuple> findByFilter(List<Integer> teamIndexes, Date begin, Date end) {
+        if (teamIndexes == null) {
+            teamIndexes = List.of(Team.values().length);
+        }
+
+        return JPA.em().createNativeQuery("select " +
+                        "v.id as id, v.happened as happened, c.name as cell, v.comment as comment " +
+                        "from vacuuming v left join cell c on v.cell_id = c.id where " +
+                        "c.team in ?1 and v.happened > ?2 and v.happened < ?3", Tuple.class)
+                .setParameter(1, teamIndexes)
+                .setParameter(2, begin)
+                .setParameter(3, end)
+                .getResultList();
     }
 
 
