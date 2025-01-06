103a579f9d3d1c48997fce9f953baeb9bbedab2e
Ilya Makarov <mortimer08@gmail.com>
2025-01-06 19:10:57 +0500

    edit query

--- app/models/information/Vacuuming.java
+++ app/models/information/Vacuuming.java
@@ -1,7 +1,8 @@
 package models.information;
 
 import common.model.information.Event;
-import models.Team;
+import controllers.information.dto.VacuumingFilter;
+import controllers.util.DateUtils;
 import models.ground.Cell;
 import play.db.jpa.JPA;
 
@@ -9,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Tuple;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -35,16 +37,19 @@ public class Vacuuming extends Event {
         return find(" select v from Vacuuming v where v.cell = ?1 order by happened desc", cell).first();
     }
 
-    public static List<Tuple> findByFilter(List<Integer> teamIndexes, Date begin, Date end) {
-        if (teamIndexes == null) {
-            teamIndexes = List.of(Team.values().length);
+    public static List<Tuple> findByFilter(VacuumingFilter f) {
+        if (f.teamIndex == null) {
+            f.teamIndex = new ArrayList<>();
         }
-
-        return JPA.em().createNativeQuery("select " +
-                        "v.id as id, v.happened as happened, c.name as cell, v.comment as comment " +
-                        "from vacuuming v left join cell c on v.cell_id = c.id where " +
-                        "c.team in ?1 and v.happened > ?2 and v.happened < ?3", Tuple.class)
-                .setParameter(1, teamIndexes)
+        final Date begin = DateUtils.plusDays(f.begin, -1);
+        final Date end = DateUtils.plusDays(f.end, 1);
+        final String query = String.format("select " +
+                "v.id as id, v.happened as happened, c.name as cell, v.comment as comment " +
+                "from vacuuming v left join cell c on v.cell_id = c.id where " +
+                "c.team in ?1 and happened > ?2 and happened < ?3 " +
+                "order by %s %s", f.sort, f.order);
+        return JPA.em().createNativeQuery(query, Tuple.class)
+                .setParameter(1, f.teamIndex)
                 .setParameter(2, begin)
                 .setParameter(3, end)
                 .getResultList();
