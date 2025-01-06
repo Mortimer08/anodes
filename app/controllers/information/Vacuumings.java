712a45e5c2c3c71ece5226d68e5403539ec24d70
Ilya Makarov <mortimer08@gmail.com>
2025-01-06 20:48:02 +0500

    edit controller

--- app/controllers/information/Vacuumings.java
+++ app/controllers/information/Vacuumings.java
@@ -1,8 +1,8 @@
 package controllers.information;
 
 import controllers.Bases;
-import controllers.util.DateUtils;
-import models.Team;
+import controllers.information.dto.VacuumingFilter;
+import controllers.information.view.VacuumingView;
 import models.information.Vacuuming;
 import play.modules.router.Get;
 import play.modules.router.Post;
@@ -15,22 +15,36 @@ public class Vacuumings extends Bases {
 
     @Get("/vacuuming")
     public static void list() {
-        final List<Integer> teamIndex = Team.getIndexes();
-        final Date today = new Date();
-        final Date begin = DateUtils.monthFirstDate(today);
-        final Date end = DateUtils.monthLastDate(today);
-        final Date start = DateUtils.plusDays(begin, -1);
-        final Date finish = DateUtils.plusDays(end, 1);
-        final List<Tuple> vacuumings = Vacuuming.findByFilter(teamIndex, start, finish);
-        render(vacuumings, begin, end);
+        final VacuumingFilter f = getNewFilter(VacuumingFilter.class);
+        final List<Tuple> vacuumings = Vacuuming.findByFilter(f);
+        render(vacuumings, f);
     }
 
-    @Post("/vacuuming/filter")
-    public static void filter(final List<Integer> teamIndex, Date begin, Date end) {
-        begin = DateUtils.plusDays(begin, -1);
-        end = DateUtils.plusDays(end, 1);
-        final List<Tuple> vacuumings = Vacuuming.findByFilter(teamIndex, begin, end);
-        render(vacuumings);
+    @Post("/vacuuming/filter/team")
+    public static void filterTeam(final List<Integer> teamIndex) {
+        final VacuumingFilter f = getNewFilter(VacuumingFilter.class);
+        f.updateIndex(teamIndex);
+        updateFilter(f);
+        final List<Tuple> vacuumings = Vacuuming.findByFilter(f);
+        render("information/Vacuumings/filter.html", vacuumings, f);
+    }
+
+    @Post("/vacuuming/filter/date")
+    public static void filterDate(final Date begin, final Date end) {
+        final VacuumingFilter f = getNewFilter(VacuumingFilter.class);
+        f.updateDates(begin, end);
+        updateFilter(f);
+        final List<Tuple> vacuumings = Vacuuming.findByFilter(f);
+        render("information/Vacuumings/filter.html", vacuumings, f);
+    }
+
+    @Post("/vacuuming/sort")
+    public static void sort(VacuumingView sort) {
+        final VacuumingFilter f = getNewFilter(VacuumingFilter.class);
+        f.setSort(sort);
+        updateFilter(f);
+        final List<Tuple> vacuumings = Vacuuming.findByFilter(f);
+        render("/information/Vacuumings/filter.html", vacuumings, f);
     }
 
 }
