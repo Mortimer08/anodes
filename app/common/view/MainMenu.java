9d5dff4109c65c73afd46dcce9da6adc84cfccb5
Ilya Makarov <mortimer08@gmail.com>
2025-01-08 21:32:41 +0500

    main menu enum

---
+++ app/common/view/MainMenu.java
@@ -0,0 +1,59 @@
+package common.view;
+
+import models.auth.User;
+
+import java.util.EnumSet;
+
+public enum MainMenu {
+
+    /*ANODES("", "&{'names.anodes'}", "@{ground.Anodes.list()}"),
+    TAKES("", "&{'takes'}", "@{ground.Takes.list()}"),
+    CELLS("", "&{'cells'}", "@{ground.Cells.list()}"),
+    ROWS("", "&{'rows'}", " @{ground.Rows.list()}"),
+    TIERS("", "&{'tiers'}", " @{ground.Tiers.list()}"),*/
+    CLEANING_TEAM_1("", "Чистка бригада 1", "/cleaning/team/1"),
+    CLEANING_TEAM_2("", "Чистка бригада 2", "/cleaning/team/2"),
+    CLEANING_TEAM_3("", "Чистка бригада 3", "/cleaning/team/3"),
+    CLEANING_TEAM_4("", "Чистка бригада 4", "/cleaning/team/4"),
+    CLEANING_TEAM_5("", "Чистка бригада 5", "/cleaning/team/5"),
+    VACUUMINGS("", "Крутежка", "/vacuuming"),
+    SCRUBBINGS("", "Подъемы", "/scrubbing"),
+    EXIT("", "Выход", "/logout"),
+    ;
+
+    public String name;
+    public String title;
+    public String url;
+
+    MainMenu(String name, String title, String url) {
+        this.name = name;
+        this.title = title;
+        this.url = url;
+    }
+
+    public static EnumSet<MainMenu> getMenu(final User user) {
+        if (user.superuser) {
+            return EnumSet.of(
+                    CLEANING_TEAM_1,
+                    CLEANING_TEAM_2,
+                    CLEANING_TEAM_3,
+                    CLEANING_TEAM_4,
+                    CLEANING_TEAM_5,
+                    VACUUMINGS,
+                    SCRUBBINGS,
+                    EXIT
+            );
+        } else {
+            return EnumSet.of(
+                    CLEANING_TEAM_1,
+                    CLEANING_TEAM_2,
+                    CLEANING_TEAM_3,
+                    CLEANING_TEAM_4,
+                    CLEANING_TEAM_5,
+                    VACUUMINGS,
+                    SCRUBBINGS,
+                    EXIT
+            );
+        }
+    }
+}
