acc38f1d19fe2990035a850764b08cebfabcdc16
Ilya Makarov <mortimer08@gmail.com>
2025-01-09 13:43:03 +0500

    change columns order

--- app/controllers/information/view/ScrubbingView.java
+++ app/controllers/information/view/ScrubbingView.java
@@ -4,9 +4,9 @@ public enum ScrubbingView {
 
     HAPPENED("happened","Дата"),
     TAKE("take","Подъем"),
-    MACHINED("machined","W703"),
     FIRST_DAMAGE("firstDamage","<10%"),
     TO_CHANGE("toChange","На замену"),
+    MACHINED("machined","W703"),
     CHANGED("changed","Заменили"),
     COMMENT("comment","Примечание"),
     ;
