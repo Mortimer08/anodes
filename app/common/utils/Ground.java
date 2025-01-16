8c847ffcf4b8b9d8035c332c27d2aeba89476c68
Ilya Makarov <mortimer08@gmail.com>
2025-01-16 19:17:39 +0500

    bootstrap create ground add active=true, logs

--- app/common/utils/Ground.java
+++ app/common/utils/Ground.java
@@ -2,6 +2,7 @@ package common.utils;
 
 import common.model.ground.Default;
 import models.ground.*;
+import play.Logger;
 
 import java.util.List;
 
@@ -11,47 +12,55 @@ public class Ground {
         createTiers();
         createRows();
         createCells();
-        /*createTakes();
-        createAnodes();*/
+        createTakes();
+        createAnodes();
     }
 
     private static void createTiers() {
+        Logger.info("create default tiers");
         for (String name : Default.TIER_NAMES) {
             final Tier tier = new Tier();
             tier.name = name;
+            tier.active = true;
             tier.save();
         }
     }
 
     private static void createRows() {
+        Logger.info("create default rows");
         final List<Tier> tiers = Tier.findAll();
         for (Tier tier : tiers) {
             for (String rowName : Default.ROW_NAMES.get(tier.name)) {
                 Row row = new Row();
                 row.name = rowName;
                 row.tier = tier;
+                row.active = true;
                 row.save();
             }
         }
     }
 
     private static void createCells() {
+        Logger.info("create default cells");
         final List<Row> rows = Row.findAll();
         for (Row row : rows) {
             for (int i = 1; i < Default.CELLS_IN_ROW; i++) {
                 final Cell cell = new Cell(i, row);
                 cell.create();
                 cell.setTeam(Default.getTeamByCell(cell));
+                cell.active = true;
                 cell.save();
             }
         }
     }
 
     private static void createTakes() {
+        Logger.info("create default takes");
         final List<Cell> cells = Cell.findAll();
         for (Cell cell : cells) {
             for (TakeNumber number : TakeNumber.values()) {
                 final Take take = new Take(cell, number);
+                take.active = true;
                 take.save();
             }
         }
@@ -62,6 +71,7 @@ public class Ground {
         for (Take take : takes) {
             for (Integer number : take.number.anodes) {
                 final Anode anode = new Anode(number, take);
+                anode.active = true;
                 anode.save();
             }
         }
