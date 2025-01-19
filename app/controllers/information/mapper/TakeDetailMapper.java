497a3532a21d3935b7b872667ac6342a692cfbd6
Ilya Makarov <mortimer08@gmail.com>
2025-01-19 18:55:09 +0500

    mapper

--- app/controllers/information/mapper/TakeDetailMapper.java
+++ app/controllers/information/mapper/TakeDetailMapper.java
@@ -1,10 +1,12 @@
 package controllers.information.mapper;
 
 import controllers.information.dto.TakeDetailDto;
+import models.ground.TakeNumber;
 import models.information.TakeScrubbing;
 import models.information.TakeDetail;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
+
 import java.util.Date;
 
 public class TakeDetailMapper {
@@ -14,7 +16,13 @@ public class TakeDetailMapper {
         takeDetail.checked = true;
         takeDetail.scrubbed = dto.moment;
         takeDetail.comment = dto.comment;
-        takeDetail.machined = dto.machined;
+        if (dto.machined != null) {
+            takeDetail.machined = TakeNumber.getQuantity(dto.takeNumber);
+            takeDetail.handled = 0;
+        } else {
+            takeDetail.machined = 0;
+            takeDetail.handled = TakeNumber.getQuantity(dto.takeNumber);
+        }
         takeDetail.firstDamage = dto.firstDamage;
         takeDetail.toChange = dto.toChange;
         takeDetail.changed = dto.changed;
@@ -29,6 +37,7 @@ public class TakeDetailMapper {
             takeDetail.scrubbed = scrubbed.toDate();
             takeDetail.comment = scrubbing.comment;
             takeDetail.machined = scrubbing.machined != null ? scrubbing.machined : takeDetail.take.getQuantity();
+            takeDetail.handled = scrubbing.handled != null ? scrubbing.handled : 0;
             takeDetail.firstDamage = scrubbing.firstDamage != null ? scrubbing.firstDamage : 0;
             takeDetail.toChange = scrubbing.toChange != null ? scrubbing.toChange : 0;
             takeDetail.changed = scrubbing.changed != null ? scrubbing.changed : 0;
@@ -38,8 +47,9 @@ public class TakeDetailMapper {
 
     private static void makeDefault(final TakeDetail takeDetail) {
         takeDetail.scrubbed = null;
-        takeDetail.comment = "-";
+        takeDetail.comment = null;
         takeDetail.machined = takeDetail.take.getQuantity();
+        takeDetail.handled = 0;
         takeDetail.firstDamage = 0;
         takeDetail.toChange = 0;
         takeDetail.changed = 0;
