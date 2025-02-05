ab74d5a558873c741cbb65587bab65bc4c47d6de
Ilya Makarov <mortimer08@gmail.com>
2025-02-05 18:44:37 +0500

    totals

--- app/controllers/information/Totals.java
+++ app/controllers/information/Totals.java
@@ -17,32 +17,34 @@ public class Totals extends Bases {
     public static void view() {
         final Date begin = DateUtils.yearFirstDate(new Date());
         final Date end = DateUtils.yearLastDate(new Date());
-        final List<Tuple> cellTerms = TotalRepo.findCellTerms();
-        final List<Tuple> takeTerms = TotalRepo.findTakeTerms();
         final Integer cellMaxTerm = TotalRepo.findCellMaxTerm();
         final Integer takeMaxTerm = TotalRepo.findTakeMaxTerm();
         final Tuple total = TotalRepo.findTakeTotal(begin, end);
         Tuple[] res = new Tuple[5];
         Tuple[] cellTerm = new Tuple[5];
+        Tuple[] takeTerm = new Tuple[5];
         for (Team team : Team.values()) {
             cellTerm[team.ordinal()] = TotalRepo.findCellTerm(team);
+            takeTerm[team.ordinal()] = TotalRepo.findTakeTerm(team);
             res[team.ordinal()] = TotalRepo.findTeamResult(team, begin, end);
         }
-        render(res, cellTerm, cellTerms, cellMaxTerm, takeTerms, takeMaxTerm, total, begin, end);
+        render(res, cellTerm, cellTerm, cellMaxTerm, takeTerm, takeMaxTerm, total, begin, end);
     }
 
 
     @Post("/total/period")
     public static void period(final Date begin, final Date end) {
-        final List<Tuple> cellTerms = TotalRepo.findCellTerms();
-        final List<Tuple> takeTerms = TotalRepo.findTakeTerms();
         final Integer cellMaxTerm = TotalRepo.findCellMaxTerm();
         final Integer takeMaxTerm = TotalRepo.findTakeMaxTerm();
         final Tuple total = TotalRepo.findTakeTotal(begin, end);
         Tuple[] res = new Tuple[5];
+        Tuple[] cellTerm = new Tuple[5];
+        Tuple[] takeTerm = new Tuple[5];
         for (Team team : Team.values()) {
+            cellTerm[team.ordinal()] = TotalRepo.findCellTerm(team);
+            takeTerm[team.ordinal()] = TotalRepo.findTakeTerm(team);
             res[team.ordinal()] = TotalRepo.findTeamResult(team, begin, end);
         }
-        render(res, cellTerms, cellMaxTerm, takeTerms, takeMaxTerm, total, begin, end);
+        render(res, cellTerm, cellMaxTerm, takeTerm, takeMaxTerm, total, begin, end);
     }
 }
--- app/controllers/information/repo/TotalRepo.java
+++ app/controllers/information/repo/TotalRepo.java
@@ -9,40 +9,6 @@ import java.util.List;
 
 public class TotalRepo {
 
-    public static List<Tuple> findTakeResults(final Date begin, final Date end) {
-        final String query = "select " +
-                "SUM(s.firstDamage) firstDamage, " +
-                "SUM(s.toChange) toChange, " +
-                "SUM(s.firstDamage) + SUM(s.toChange) totalDamage, " +
-                "c.team team " +
-                "from take t " +
-                "left join take_scrubbing s " +
-                "on t.lastScrubbing_id = s.id " +
-                "left join cell c " +
-                "on t.cell_id = c.id " +
-                "where s.happened > ?1 " +
-                "and s.happened < ?2 " +
-                "group by c.team " +
-                "order by c.team";
-        return JPA.em().createNativeQuery(query, Tuple.class)
-                .setParameter(1, begin)
-                .setParameter(2, end)
-                .getResultList();
-    }
-
-    public static List<Tuple> findCellTerms() {
-        final String query = "select " +
-                "DATEDIFF(NOW(), MIN(v.happened)) cellTerm, " +
-                "c.team team " +
-                "from cell c " +
-                "join vacuuming v " +
-                "on c.lastVacuuming_id = v.id " +
-                "group by c.team " +
-                "order by c.team";
-        return JPA.em().createNativeQuery(query, Tuple.class)
-                .getResultList();
-    }
-
     public static Tuple findCellTerm(final Team team) {
         final String query = "select " +
                 "DATEDIFF(NOW(), MIN(v.happened)) cellTerm, " +
@@ -66,19 +32,17 @@ public class TotalRepo {
                 .getSingleResult()).intValue();
     }
 
-    public static List<Tuple> findTakeTerms() {
+    public static Tuple findTakeTerm(final Team team) {
         final String query = "select " +
                 "DATEDIFF(NOW(), MIN(s.happened)) takeTerm, " +
                 "c.team team " +
                 "from take t " +
-                "left join cell c " +
-                "on t.cell_id = c.id " +
-                "join take_scrubbing s " +
-                "on t.lastScrubbing_id = s.id " +
-                "group by c.team " +
-                "order by c.team";
-        return JPA.em().createNativeQuery(query, Tuple.class)
-                .getResultList();
+                "left join cell c on t.cell_id = c.id " +
+                "join take_scrubbing s on t.lastScrubbing_id = s.id " +
+                "where c.team = ?1";
+        return (Tuple) JPA.em().createNativeQuery(query, Tuple.class)
+                .setParameter(1, team)
+                .getSingleResult();
     }
 
     public static Integer findTakeMaxTerm() {
